2c28d6544714fb39d6b4ec15b210b64536adee89
Jun Jun M. Zaragosa <144114300+jzaragosa06@users.noreply.github.com>
2024-07-10 09:11:57 +0000

    addded multiple refit for forecasting

--- forecast/forecast_univariate.py
+++ forecast/forecast_univariate.py
@@ -15,6 +15,8 @@ from sklearn.ensemble import StackingRegressor
 from sklearn.linear_model import LinearRegression
 from sklearn.svm import SVR
 
+from sklearn.model_selection import train_test_split
+
 #this is relative import
 # from seasonality_analysis import *
 from models.stacking import *
@@ -67,6 +69,101 @@ def forcast_uni(df_arg, lag_value, steps_value,freq, forecast_method = 'without_
 
         #then create a new dataframe of result
         forecast_df = pd.DataFrame(data=pred.values, index=new_indices, columns=['target'])
+    else: 
+
+        temp_data = df.copy()
+        last_col = df.columns[-1]
+
+        forecaster = ForecasterAutoreg(
+            regressor=stacking_regressor, 
+            lags = lag_value,
+            transformer_y= StandardScaler()
+        )
+
+
+        pred_values = []
+        pred_indices = []
+
+        for i in range(steps_value):
+            #fit
+            forecaster.fit(temp_data.iloc[:, -1])
+
+            pred_i = pd.DataFrame(forecaster.predict(steps = 1))
+            pred_values.append(pred_i.iloc[0,0])
+            
+
+            if freq == 'D':
+                new_index = last_index + pd.DateOffset(days=1)
+            elif freq == 'W':
+                new_index = last_index + pd.DateOffset(weeks=1)
+            elif freq == 'M':
+                new_index = last_index + pd.DateOffset(months=1)
+            elif freq == 'Q':
+                new_index = last_index + pd.DateOffset(months=3)
+            elif freq == 'Y':
+                new_index = last_index + pd.DateOffset(years=1)
+            else:
+                raise ValueError(f"Frequency '{freq}' is not supported")
+            
+            pred_indices.append(new_index)
+
+            temp_data.loc[pd.DatetimeIndex(new_index), last_col] = pred_i.iloc[0,0]
+        
+        pred_indices = pd.DatetimeIndex(pred_indices)
+        #store as a dataframe
+        forecast_df = pd.DataFrame(data=pred_values, index=pred_indices, columns=['target'])
+
+            
+
+
+def evaluate_model(df_arg, lag_value, steps_value,freq, forecast_method = 'without_refit'):
+    df = df_arg.copy()
+
+    test_size = 0.2
+    test_samples = int(test_size * len(df))
+    train_data, test_data = df[:-test_samples], df[-test_samples]
+
+    #get the model
+    stacking_regressor = build_staking_regressor()
+
+    forecaster = ForecasterAutoreg(
+        regressor=stacking_regressor, 
+        lags = lag_value,
+        transformer_y= StandardScaler()
+    )
+
+
+    if forecast_method == 'without_refit': 
+        #metrics and predictions are on the testing
+        metric, prediction = backtesting_forecaster(
+            forecaster=forecaster, 
+            y=df.iloc[:-1],
+            initial_train_size=len(train_data),
+            steps = 12, 
+            refit = False, 
+            fixed_train_size=True,
+            metric='mean_squared_error', 
+            n_jobs='auto', 
+            verbose=False, 
+        )
+
+        return metric, prediction
+    else: 
+        #metrics and predictions are on the testing
+        metric, prediction = backtesting_forecaster(
+            forecaster=forecaster, 
+            y=df.iloc[:-1],
+            initial_train_size=len(train_data),
+            steps = 12, 
+            refit = True, 
+            fixed_train_size=False, 
+            metric='mean_squared_error', 
+            n_jobs='auto', 
+            verbose=False, 
+        )
+
+        return metric, prediction
+
 
 
 
