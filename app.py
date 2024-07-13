20676b275d0d768466b127a5616505d4d9601179
jzaragosa06 <junjunzaragosa2309@gmail.com>
2024-07-13 22:12:01 +0800

    updated the forecast uni. updated app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 from utility.lags import *
+from forecast.forecast_univariate import *
 
 app = Flask(__name__)
 # this will disable the CORS protection for the proceeding routes.
@@ -11,7 +12,7 @@ CORS(app)
 
 
 @app.route("/handle_submit_uni_ts", methods=["POST"])
-def handle_submit_uni_ts(): 
+def handle_submit_uni_ts():
     if "inputFile" not in request.files:
         print("error1")
         return jsonify({"message": "No file part in the request"}), 400
@@ -24,20 +25,43 @@ def handle_submit_uni_ts():
 
     if file:
         # extract the other variables.
-        indexType = request.form.get('indexType')
-        freq = request.form.get('freq')
-        steps = request.form.get('steps')
+        indexType = request.form.get("indexType")
+        freq = request.form.get("freq")
+        steps = request.form.get("steps")
+        forecastMethod = request.form.get("forecastMethod")
 
-        df = pd.read_csv(file, index_col= 0, parse_dates=True)
+        df = pd.read_csv(file, index_col=0, parse_dates=True)
         # enforce the frequency
         df = df.asfreq(freq)
 
+        print(df)
+
         # extract the
-        lag = sig_lag(df, 50, ts_type='univariate')
-        
-        result = forecast_uni(df, lag, steps, freq, forecast_method="without_refit")
-        
-        
+        lag = sig_lag(df, 50, ts_type="univariate")
+        print(lag)
+
+        if forecastMethod == "without_refit":
+            result = forecast_uni(
+                df, int(lag), int(steps), freq, forecast_method="without_refit"
+            )
+            print(f"predicted.train on all: {result}")
+
+            metric, pred = evaluate_model(
+                df, int(lag), int(steps), freq, forecast_method="without_refit"
+            )
+
+            print(f"mse on training data: {metric}. predicted value on test: {pred}")
+        else:
+            result = forecast_uni(
+                df, int(lag), int(steps), freq, forecast_method="with_refit"
+            )
+            print(f"predicted.train on all: {result}")
+
+            metric, pred = evaluate_model(
+                df, int(lag), int(steps), freq, forecast_method="with_refit"
+            )
+
+            print(f"mse on training data: {metric}. predicted value on test: {pred}")
 
 
 @app.route("/handle_submit_multi_ts", methods=["POST"])
--- utility/lags.py
+++ utility/lags.py
@@ -1,29 +1,29 @@
 import pandas as pd
-import numpy as np 
+import numpy as np
 import statsmodels.api as sm
 
 
-def sig_lag(df, max_lag, ts_type = 'univariate'): 
-    if ts_type == 'univariate':
-      acf = sm.tsa.stattools.acf(df, nlags=max_lag)
-      acf_sig_lags = np.where(np.abs(acf) > 2 / np.sqrt(len(df)))[0]
-
-      acf_sig_lag = acf_sig_lags[len(acf_sig_lags) - 1]
-
-      return acf_sig_lag
-    else: 
-      #for each of the vriables, we'll find the significant lag. 
-      #we'll just align it to the order of the columns in df. 
-
-      acf_lags_muli = []
-      for i in range(len(df.columns)): 
-        #we'll start with features and end with target. 
-        acf = sm.tsa.stattools.acf(df.iloc[:, i], nlags = max_lag)
+def sig_lag(df, max_lag, ts_type="univariate"):
+    if ts_type == "univariate":
+        acf = sm.tsa.stattools.acf(df, nlags=max_lag)
         acf_sig_lags = np.where(np.abs(acf) > 2 / np.sqrt(len(df)))[0]
-        
-        acf_sig_lag = acf_sig_lags[len(acf_sig_lags) - 1]
-       
-        acf_lags_muli.append(acf_sig_lag)
-        # acf_lags_muli.append({f"feature_{i}": acf_sig_lag})
 
-      return acf_lags_muli
+        acf_sig_lag = acf_sig_lags[len(acf_sig_lags) - 1]
+
+        return acf_sig_lag
+    else:
+        # for each of the vriables, we'll find the significant lag.
+        # we'll just align it to the order of the columns in df.
+
+        acf_lags_muli = []
+        for i in range(len(df.columns)):
+            # we'll start with features and end with target.
+            acf = sm.tsa.stattools.acf(df.iloc[:, i], nlags=max_lag)
+            acf_sig_lags = np.where(np.abs(acf) > 2 / np.sqrt(len(df)))[0]
+
+            acf_sig_lag = acf_sig_lags[len(acf_sig_lags) - 1]
+
+            acf_lags_muli.append(acf_sig_lag)
+            # acf_lags_muli.append({f"feature_{i}": acf_sig_lag})
+
+        return acf_lags_muli
