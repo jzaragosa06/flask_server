38b245a9a200537f195226e856a89d6d5d2bdf08
jzaragosa06 <junjunzaragosa2309@gmail.com>
2024-10-12 10:21:45 +0800

    remove unecessary files and comments.

--- app/resources/trend_prophet/trend.py
+++ app/resources/trend_prophet/trend.py
@@ -2,7 +2,6 @@ import pandas as pd
 from prophet import Prophet
 import matplotlib.pyplot as plt
 from datetime import datetime
-from tabulate import tabulate
 
 
 def compute_trend_prophet(df_arg, date_column, value_columns, freq):
@@ -43,46 +42,4 @@ def compute_trend_prophet(df_arg, date_column, value_columns, freq):
         # storing the dataframes (including the date and the treend) into a dictionary of dataframe, for each variable.
         trend_results[value_column] = forecast[components]
 
-        # # Plot the decomposition graph (trend, seasonality)
-        # model.plot_components(forecast)
-        # plt.suptitle(f"Seasonality Components for {value_column}")
-        # plt.show()
-
-    #     # Extract the seasonality components that are present
-    #     seasonality_df = forecast[components]
-    #     seasonality_results[value_column] = seasonality_df
-
-    #     # =====================================================================================
-    #     # Compute and extract specific seasonal components like yearly or weekly
-    #     for component in components:
-    #         if component == "yearly":
-    #             # Compute yearly seasonality for a Jan 1 - Dec 31 sequence of dates.
-    #             days = pd.date_range(start='2017-01-01', periods=365)
-    #             df_y = seasonality_plot_df(model, days)
-    #             seas = model.predict_seasonal_components(df_y)
-    #             seas_returned_values[value_column] = seas
-
-    #     # =====================================================================================
-
-    # return seasonality_results, seas_returned_values
     return trend_results
-
-
-# # Example usage
-# # df = pd.read_csv(r"test/data/candy_production.csv", index_col=0, parse_dates=True)
-# df = pd.read_csv(r"test/data/apple2.csv", index_col=0, parse_dates=True)
-
-# df.index = pd.to_datetime(
-#     df.index, errors="coerce"
-# )  # Convert index to datetime explicitly
-
-# date_column = df.index.name
-# value_columns = df.columns.to_list()
-
-# # Compute seasonality
-# trend_results = compute_trend_prophet(
-#     df_arg=df, date_column=date_column, value_columns=value_columns, freq="M"
-# )
-
-
-# print(trend_results)
